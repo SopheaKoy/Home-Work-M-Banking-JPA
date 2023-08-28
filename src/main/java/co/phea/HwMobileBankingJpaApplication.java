0be62975291a447a54085aef2bac1af1d3626bc1
SopheaKoy <koysophea09@gmail.com>
2023-08-29 01:36:44 +0700

    dependency

---
+++ src/main/java/co/phea/HwMobileBankingJpaApplication.java
@@ -0,0 +1,13 @@
+package co.phea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwMobileBankingJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwMobileBankingJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/phea/HwMobileBankingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.phea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwMobileBankingJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
